4628952a116bf45884998d8e47c55b92462478d9
lukyans <sukyans@gmail.com>
2018-04-06 12:55:37 -0700

    remove first and last name from rides spec

--- spec/features/guest_ride_page_spec.rb
+++ spec/features/guest_ride_page_spec.rb
@@ -1,22 +1,18 @@
 require "rails_helper"
 
 RSpec.feature "Rides", :type => :feature do
-  # let(:user) { create(:user) }
+  let(:user) { create(:user) }
   # when I visit ride page
   #   I can see a ride form
 
   scenario "Guest should be able to visit ride page" do
     visit rides_path
-    fill_in 'First name', with: 'John'
-    fill_in 'Last name', with: 'Doe'
     fill_in 'Airport', with: 'JFK'
     fill_in 'Flight Number', with: '123456'
     choose 'Arriving'
     select 'Two', :from => 'select_spots'
     fill_in 'Date', with: '04/05/2018'
 
-    expect(page).to have_selector("input[value='John']")
-    expect(page).to have_selector("input[value='Doe']")
     expect(page).to have_selector("input[value='JFK']")
     expect(page).to have_selector("input[value='123456']")
     expect(page).to have_selector("input[value='04/05/2018']")
