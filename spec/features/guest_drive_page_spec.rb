11db0189e4bcf647e69f9e2f75e478a69fa6cdb3
lukyans <sukyans@gmail.com>
2018-04-06 12:04:42 -0700

    remove name input from spec for drive

--- spec/features/guest_drive_page_spec.rb
+++ spec/features/guest_drive_page_spec.rb
@@ -5,14 +5,10 @@ RSpec.feature "Drives", :type => :feature do
   #   I can see a drive form
   scenario "Guest should be able to visit drive page" do
     visit drives_path
-    fill_in 'First name', with: 'John'
-    fill_in 'Last name', with: 'Doe'
     fill_in 'Airport', with: 'JFK'
     select 'Two', :from => 'select_spots'
     fill_in 'Date', with: '04/05/2018'
 
-    expect(page).to have_selector("input[value='John']")
-    expect(page).to have_selector("input[value='Doe']")
     expect(page).to have_selector("input[value='JFK']")
     expect(page).to have_selector("input[value='04/05/2018']")
 
