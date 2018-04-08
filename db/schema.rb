f8b7458ae2b37e625ec70c255a48e140e9273436
lukyans <sukyans@gmail.com>
2018-04-08 15:55:18 -0700

    add user:reference to rides

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408224646) do
+ActiveRecord::Schema.define(version: 20180408225432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20180408224646) do
     t.string "airport"
     t.integer "spots"
     t.datetime "date"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_drives_on_user_id"
   end
 
   create_table "rides", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema.define(version: 20180408224646) do
     t.string "traveling_status"
     t.integer "spot"
     t.datetime "date"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_rides_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +53,6 @@ ActiveRecord::Schema.define(version: 20180408224646) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "drives", "users"
+  add_foreign_key "rides", "users"
 end
