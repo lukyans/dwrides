94a98a6d05366e156041cfe90658913639ec5ff6
lukyans <sukyans@gmail.com>
2018-04-09 12:50:03 -0700

    add validation test to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:rides) }
+  it { should have_many(:drives) }
 end
