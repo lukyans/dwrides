c7db0d1a90c26f08bbc000087ddf4e66f8b4a238
lukyans <sukyans@gmail.com>
2018-04-09 12:47:14 -0700

    add ride validation to model

---
+++ app/models/ride.rb
@@ -0,0 +1,9 @@
+class Ride < ApplicationRecord
+  validates :airport, presence: true
+  validates :flight_number, presence: true
+  validates :traveling_status, presence: true
+  validates :spot, presence: true
+  validates :date, presence: true
+
+  belongs_to :user
+end
