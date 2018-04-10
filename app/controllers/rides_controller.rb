a30945949df9d76dda8595d12f5b43ac40b3cf55
lukyans <sukyans@gmail.com>
2018-04-09 17:03:55 -0700

    add create for ride controller

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,11 +1,20 @@
 class RidesController < ApplicationController
   def index
+    @ride = Ride.new
   end
 
   def create
+    @ride = Ride.new(ride_params)
+    @ride.save
     # if ride.save
     #   flash[:success] = "Welcome to the Sample App!"
     #   redirect_to root_path
     # else
+  end
+
+private
+
+  def ride_params
+    params.require(:ride).permit(:id, :airport, :flight_number, :traveling_status, :spot, :date)
   end
 end
