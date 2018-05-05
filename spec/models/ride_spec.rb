90e96718cfe7216ff87a8400f08396c1074e8a5b
Sergey <sukyans@gmail.com>
2018-05-05 10:53:48 -0700

    should not validate presence of flight number

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Ride, type: :model do
   it { should validate_presence_of(:airport) }
-  it { should validate_presence_of(:flight_number) }
+  # it { should validate_presence_of(:flight_number) }
   it { should validate_presence_of(:traveling_status) }
   it { should validate_presence_of(:spot) }
   it { should validate_presence_of(:date) }
