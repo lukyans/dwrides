8a7618c42a3a95274757ab554dbf160c3d4f8aa6
Sergey <sukyans@gmail.com>
2018-05-08 19:49:27 -0700

    create method matching_with_drive in drive.rb

--- app/models/drive.rb
+++ app/models/drive.rb
@@ -4,4 +4,10 @@ class Drive < ApplicationRecord
   validates :date, presence: true
 
   belongs_to :user
+
+  def matching_with_rides?
+    Ride.all.each do |ride|
+       return true if Date.parse(ride.date.to_s) == Date.parse(self.date.to_s) && ride.airport == self.airport
+    end
+  end
 end
