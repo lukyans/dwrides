4ac06d2440af4f4557f53e1ff57a91898a30cd91
Sergey <sukyans@gmail.com>
2018-05-10 18:17:39 -0700

    add cloudinary initializer to configurations to use ruby cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dbo1mgpii'
+  config.api_key = '478588297445712'
+  config.api_secret = 'km57NlSO25QY3nMRaLqjv6iZpZM'
+  config.secure = true
+  config.cdn_subdomain = true
+end
