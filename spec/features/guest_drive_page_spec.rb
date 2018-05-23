18ee366832ed4d73d450a831c5e2dfd420fb9363
Sergey <sukyans@gmail.com>
2018-05-22 23:13:47 -0700

    drive page spec

--- spec/features/guest_drive_page_spec.rb
+++ spec/features/guest_drive_page_spec.rb
@@ -1,11 +1,21 @@
 require "rails_helper"
 
 RSpec.feature "Drives", :type => :feature do
-  let(:user) { create(:user) }
+  # let(:user) { create(:user) }
   # when I visit drive page
   #   I can see a drive form
   scenario "Guest should be able to visit drive page" do
+    user = FactoryBot.create(:user, :password => "123456")
     visit drives_path
+    # visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: "123456"
+    click_button('Login')
+    # visit drives_path
+    # expect(current_path).to eq(drives_path)
+    # binding.pry
+    save_and_open_page
+
     fill_in 'Airport', with: 'JFK'
     select 'Two', :from => 'select_spots'
     fill_in 'Date', with: '04/05/2018'
