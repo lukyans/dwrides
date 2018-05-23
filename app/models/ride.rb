e1a5826a1a093b24d931053666b882ba059efc55
Sergey <sukyans@gmail.com>
2018-05-23 12:59:04 -0700

    create ride association to courses

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,10 +6,15 @@ class Ride < ApplicationRecord
   validates :date, presence: true
 
   belongs_to :user
+  has_many :courses
 
   def matching_with_drives?
     Drive.all.each do |drive|
       return true if Date.parse(self.date.to_s) == Date.parse(drive.date.to_s) && self.airport == drive.airport
     end
+  end
+
+  def format_time
+    self.time.strftime("%b %e")
   end
 end
