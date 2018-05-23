49d7cd57c742fe0df37e7cd2bd94dd1cbd6b4b8b
Sergey <sukyans@gmail.com>
2018-05-23 14:46:07 -0700

    add create action to courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,25 @@
 class CoursesController < ApplicationController
   def index
   end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.user = current_user
+    if @course.save
+      flash[:success] = "Course has been added."
+      redirect_to rides_path
+    else
+      render :action => :new
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :location, :airport, :date)
+  end
 end
