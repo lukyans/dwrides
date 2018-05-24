278847e34a2aa05464a9382ca231620f07b868bd
Sergey <sukyans@gmail.com>
2018-05-23 21:40:36 -0700

    put course info in one string

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,18 @@
 class Course < ApplicationRecord
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :airport, presence: true
+  validates :date, presence: true
+
   belongs_to :ride
   belongs_to :drive
   belongs_to :user
+
+  def course_details
+    "#{format_time}, #{self.location}, #{self.name}"
+  end
+
+  def format_time
+    self.date.strftime("%b %e")
+  end
 end
