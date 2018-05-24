d295be00af3b9ac180ab91c21af8e4a1f71aa952
Sergey <sukyans@gmail.com>
2018-05-23 21:46:21 -0700

    uncomment drives routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,12 @@ Rails.application.routes.draw do
   # resources :home, only: [:index]
   root to: "home#index"
 
-  # resources :rides, only: [:index, :create] do
-  #   collection do
-  #     get :requested
-  #     get :available
-  #   end
-  # end
+  resources :drives, only: [:index, :create] do
+    collection do
+      get :requested
+      get :available
+    end
+  end
   resources :rides, only: [:index, :create] do
     collection do
       get :requested
